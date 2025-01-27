db63d5dd851eae8d7be8f33ee356acc36b97d72b
emorri27 <164799513+emorri27@users.noreply.github.com>
2025-01-27 13:14:38 -0500

    finished module 2 lab

--- src/LAB_02/DTCCArrayList.java
+++ src/LAB_02/DTCCArrayList.java
@@ -1,3 +1,13 @@
+/*
+Course: CIS211-400
+Author: Elliott Morris
+Assignment: Module 2, Lab: Array List
+Date: 1/21/2025
+Description: Program defines a DTCCArrayList class for managing integers in a dynamically sized array
+
+I wrote all the code submitted, and I have provided citations and references where appropriate.
+ */
+
 package LAB_02;
 
 import java.util.List;
@@ -18,18 +28,20 @@ public class DTCCArrayList {
         if (size < array.length) {
             array[size] = value;
             size++;
+        } else {
+            System.out.println("Error. List is full.");
         }
     }
     public int get(int index) {
-        if (size >= index) {
+        if (index < size && index >= 0) {
             return array[index];
         } else {
-            throw new IllegalArgumentException("Index out of bounds.");
+            throw new IndexOutOfBoundsException("Index out of bounds.");
         }
     }
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder(); // I got this from
+        StringBuilder sb = new StringBuilder(); // I got this from observing the other Java classes
         sb.append("[");
         for (int i = 0; i < size; i++) {
             sb.append(array[i]);
