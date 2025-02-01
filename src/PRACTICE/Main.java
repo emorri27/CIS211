576854548540636d8b9f4ce9bfb50f6affde5751
emorri27 <164799513+emorri27@users.noreply.github.com>
2025-01-31 19:21:25 -0500

    practice

--- src/PRACTICE/Main.java
+++ src/PRACTICE/Main.java
@@ -4,50 +4,34 @@ This program is just for practicing.
 
 package PRACTICE;
 
-import javax.lang.model.type.NullType;
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> array = new ArrayList<Integer>();
-        array.add(1);
-        array.add(2);
-        System.out.println(array.toString());
-
-//        String braces = "[()]{}";
-//        ArrayList<Character> chars = new ArrayList<Character>();
-//
-//        for (int i = 0; i < braces.length(); i++) {
-//            chars.add(braces.charAt(i));
-//        }
-//
-//        for (int i = 0; i < chars.size(); i++) {
-//            checkBrace(chars.get(i));
-//        }
+        System.out.println(isPalindrome(141));
     }
 
-    public static boolean checkBrace(char brace) {
-        switch (brace) {
-            case ']':
-                System.out.println("CLOSED.");
-                return false;
-            case ')':
-                System.out.println("CLOSED.");
-                return false;
-            case '}':
-                System.out.println("CLOSED.");
-                return false;
-            case '[':
-                System.out.println("OPEN BRACE");
-                return true;
-            case '(':
-                System.out.println("OPEN BRACE");
-                return true;
-            case '{':
-                System.out.println("OPEN BRACE");
-                return true;
-            default:
-                return true;
+    public static boolean isPalindrome(int num) {
+        String[] digits = String.valueOf(num).split("");
+        int size = digits.length;
+
+        switch (size % 2) {
+            case 0:
+                for (int i = 0; i < size; i++) {
+                    for (int j = i + 1; j < size; j++) {
+                        if (digits[i] != digits[j]) {
+                            return false;
+                        }
+                    }
+                }
+            case 1:
+                int middle = (size - 1) / 2;
+                for (int i = 0; i < middle; i++) {
+                    for (int j = size; j > middle; j--) {
+                            if (digits[i] != digits[j]) {
+                                return false;
+                            }
+                    }
+                }
         }
+        return true;
     }
 }
