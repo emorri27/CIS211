979ffee06af7570c2bceb2a7a28e01e07f853e82
emorri27 <emorri27@dtcc.edu>
2025-02-25 14:52:19 -0500

    LAB 6 start

--- src/PRACTICE/Main.java
+++ src/PRACTICE/Main.java
@@ -8,10 +8,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.add("A");
+        dll.add("B");
+        dll.add("C");
+        dll.add("D");
+        dll.add("E");
 
+        dll.remove(1);
     }
-
-//    public static String fizzBuzz(int num) {
-//
-//    }
 }
