a0fadd05ea8e7e0b6b200b59f0660d07302899b8
emorri27 <164799513+emorri27@users.noreply.github.com>
2025-03-18 13:10:07 -0400

    assignment 6

---
+++ src/ASGN_06/DoublyLinkedList.java
@@ -0,0 +1,43 @@
+package ASGN_06;
+
+public class DoublyLinkedList<E> implements Deque<E> {
+    @Override
+    public void addBack(E it) throws IllegalStateException {
+
+    }
+
+    @Override
+    public void addFront(E it) throws IllegalStateException {
+
+    }
+
+    @Override
+    public E removeFront() {
+        return null;
+    }
+
+    @Override
+    public E removeBack() {
+        return null;
+    }
+
+    @Override
+    public E getFront() {
+        return null;
+    }
+
+    @Override
+    public E getBack() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int length() {
+        return 0;
+    }
+}
--- src/ASGN_06/PalindromeChecker.java
+++ src/ASGN_06/PalindromeChecker.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 public class PalindromeChecker {
     public static void main(String[] args) {
         DequeDoublyLinkedList DDLL = new DequeDoublyLinkedList();
-        DDLL.addFront("apple");
-        DDLL.addBack("banana");
-        DDLL.addFront("orange");
 
         Scanner keys = new Scanner(System.in);
         boolean continueMenu = true;
@@ -16,12 +13,17 @@ public class PalindromeChecker {
             System.out.println("Note: This palindrome checker is case sensitive.");
             System.out.println("Enter word: ");
             String word = keys.nextLine();
-            System.out.println("Is palindrome: " + isPalindrome(word));
+            String[] chars = word.split("");
+            for (int i = 0; i < chars.length; i++) {
+                DDLL.addBack(chars[i]);
+            }
+            System.out.println("Is palindrome: " + isPalindrome(DDLL));
         }
     }
 
-    public static boolean isPalindrome(String word) {
-
-        return true;
+    public static boolean isPalindrome(DequeDoublyLinkedList wordSplit) {
+        if (wordSplit.length() == 0 || wordSplit.length() == 1) return true;
+        if (!wordSplit.getFront().equals(wordSplit.getBack())) return false;
+        return isPalindrome(wordSplit);
     }
 }
